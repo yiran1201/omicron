e4c6107466e3f81cbe5a3dc9a059a1ba79335794
yiran1201 <yiranchen1201@gmail.com>
2020-08-08 12:01:03 -0700

    update pratice for generate random invoice

--- _backend/routes/invoice-route.js
+++ _backend/routes/invoice-route.js
@@ -2,7 +2,47 @@ import { Router } from "express";
 import InvoiceModel from '../models/invoice-models'
 import { Types } from "mongoose";
 
+const choice = arr => {
+  const randomIndex1 = Math.floor(Math.random() * arr.length)
+  return arr[randomIndex1]
+}
+
+const randomPrice = () => {
+  const p = Math.floor(Math.random()*1500)
+  return p.toFixed(2)
+
+}
+
+const randomQuantity = () => {
+  const q = Math.floor(Math.random()* 800)
+  return q
+}
+
+const randomDate = () => {
+  const letters = "Jan"
+  const numbers = '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31'
+  const numbersFinal = numbers.split(',')
+  const selectNumber = choice(numbersFinal)
+  return letters + selectNumber
+
+}
 const router = Router()
+
+router.get("/", async (request, response) => {
+  console.log('***Get /api/invoice')
+  for (let i = 0; i < 10; i++) {
+    const invoiceDocument = new InvoiceModel({
+      price: randomPrice(),
+      quantity: randomQuantity(),
+      date: randomDate()
+    })
+    await invoiceDocument.save()
+  }
+  response.status(200).json('Saved a invoice')
+})
+
+
+
 
 router.get('/all', async (request, response) => {
   console.log('***GET/api/invoice/all')
