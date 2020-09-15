520a424e97519de0a8c39ce761011f575d702570
yiran1201 <yiranchen1201@gmail.com>
2020-09-14 22:16:01 -0700

    update invoice API

--- _backend/routes/invoice-route.js
+++ _backend/routes/invoice-route.js
@@ -10,13 +10,31 @@ const print = (type, route) => {
   console.log(`${type} ${'*'.repeat(6)} ${route} ${'*'.repeat(6)} ${time}`);
 };
 
-router.post('/invoice', (request, response) => {
+router.post('/invoice', async(request, response) => {
   print('POST', '/api/invoice/');
-  const invoiceDocument=new invoiceModel({
-    face:request.body.face
+  const invoiceDocument = new invoiceModel({
+    face: {
+      name: request.body.face.name,
+      source: request.body.face.source,
+      price: Number(request.body.face.price),
+    },
+    band: {
+      name: request.body.band.name,
+      source: request.body.band.source,
+      case_color: request.body.band.caseColor,
+      background: request.body.band.background,
+      price: Number(request.body.band.price),
+    },
+    warranty: {
+      name: request.body.warranty.name,
+      price: Number(request.body.warranty.price),
+    },
+  });
+  const data= await invoiceDocument.save()
+  response.status(200).json(data._id)
 
-  })
 });
+
 
 router.get('/invoice/:id', (request, response) => {
   const invoiceId = request.params.id;
